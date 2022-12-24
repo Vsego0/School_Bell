d79c8685511338a1df2b199afb4bce052c440dcd
Vsego0 <daniel.ast2007@gmail.com>
2022-12-25 01:11:47 +0300

    add mp3

--- project/music.cpp
+++ project/music.cpp
@@ -1,80 +1,37 @@
 #include <iostream>
-#include <Windows.h>
+#include "windows.h"
 #include "music.h"
+#include "audiere.h"
+#include <conio.h>
 
 Music::Music() {};
 
 void Music::play1()
 {
-    Beep(247, 500);
-    Beep(417, 500);
-    Beep(417, 500);
-    Beep(370, 500);
-    Beep(417, 500);
-    Beep(329, 500);
-    Beep(247, 500);
-    Beep(247, 500);
-    Beep(247, 500);
-    Beep(417, 500);
-    Beep(417, 500);
-    Beep(370, 500);
-    Beep(497, 500);
-    Beep(417, 500);
-    Sleep(500);
-    Beep(497, 500);
-    Beep(277, 500);
-    Beep(277, 500);
-    Beep(440, 500);
-    Beep(440, 500);
-    Beep(417, 500);
-    Beep(370, 500);
-    Beep(329, 500);
-    Beep(247, 500);
-    Beep(417, 500);
-    Beep(417, 500);
-    Beep(370, 500);
-    Beep(417, 500);
-    Beep(329, 500);
+    using namespace audiere;
+    int x;
+    AudioDevicePtr device = OpenDevice();
+    OutputStreamPtr sound = OpenSound(device, "track1.mp3", false);
+    sound->play();
+    std::cin >> x;
 };
 
 void Music::play2()
 {
-    int a = 750;
-
-    Beep(392, a);
-    Beep(392, a);
-    Beep(392, a);
-    Beep(311, a * 0.75);
-    Beep(466, a * 0.25);
-
-    Beep(392, a);
-    Beep(311, a * 0.75);
-    Beep(466, a * 0.25);
-    Beep(392, a * 2);
-
-    Beep(587, a);
-    Beep(587, a);
-    Beep(587, a);
-    Beep(622, a * 0.75);
-    Beep(466, a * 0.25);
-
-    Beep(369, a);
-    Beep(311, a * 0.75);
-    Beep(466, a * 0.25);
-    Beep(392, a * 2);
+    using namespace audiere;
+    int x;
+    AudioDevicePtr device = OpenDevice();
+    OutputStreamPtr sound = OpenSound(device, "track2.mp3", false);
+    sound->play();
+    std::cin >> x;
 }
 
 void Music::play3()
 {
-    Beep(659.26, 200);
-    Beep(659.26, 200);
-    Sleep(200);
-    Beep(659.26, 200);
-    Sleep(100);
-    Beep(523.26, 200);
-    Beep(659.26, 200);
-    Sleep(200);
-    Beep(783.98, 200);
-    Sleep(400);
-    Beep(391.99, 200);
+    using namespace audiere;
+    int x;
+    AudioDevicePtr device = OpenDevice();
+    OutputStreamPtr sound = OpenSound(device, "track3.mp3", false);
+    sound->play();
+    std::cin >> x;
 }
